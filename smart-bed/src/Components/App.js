4b497155404d92a628520395a5d3933cadc25da0
rohan -fa <rohanfarzna@gmail.com>
2022-08-30 12:45:49 +1000

    initiated login part working

---
+++ smart-bed/src/Components/App.js
@@ -0,0 +1,25 @@
+import { useState, useEffect } from 'react'
+import supabase from '../supabaseClient'
+import Auth from '../Auth'
+
+export default function App() {
+  const [session, setSession] = useState(null)
+
+  useEffect(() => {
+    supabase.auth.getSession().then(({ data: { session } }) => {
+      setSession(session)
+    })
+
+    supabase.auth.onAuthStateChange((_event, session) => {
+      setSession(session)
+    })
+  }, [])
+
+  return (
+    <div className="container" style={{ padding: '50px 0 100px 0' }}>
+      
+        <Auth />
+      
+    </div>
+  )
+}
