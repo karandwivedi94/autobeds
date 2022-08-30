efedb4c95bd51b4953085cdf10fa112745994102
rohan -fa <rohanfarzna@gmail.com>
2022-08-30 16:17:38 +1000

    updated SignIn form

--- smart-bed/src/Components/App.js
+++ smart-bed/src/Components/App.js
@@ -18,8 +18,7 @@ export default function App() {
 
   return (
     <div className="container" style={{ padding: '50px 0 100px 0' }}>
-        <Auth />
-        
+        <Auth />    
     </div>
   )
 }
